0e2b2405cf6b7378730f95820ef33b5bd441ca78
unknown <naultmichael1@gmail.com>
2019-07-18 17:12:07 -0400

    buttons need work

---
+++ models/Article.js
@@ -0,0 +1,40 @@
+//Require Mongoose
+const mongoose = require("mongoose");
+const Note = require("./Note");
+// Create Schema class
+var Schema = mongoose.Schema;
+
+// Create article schema
+var ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    //this is for an Image
+    imgUrl: {
+        type: String,
+        required: true
+    },
+    saved: {
+        type: Boolean,
+        default: false
+    },
+    notes: [{
+        type: Schema.Types.ObjectId,
+        ref: "Note"
+    }]
+
+});
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+//Export the model
+module.exports = Article;
